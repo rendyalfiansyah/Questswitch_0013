da7f2125af846356289a4048cdf3344dfd11f415
rendyalfiansyah <rendyalfiasyah356@gmail.com>
2025-11-04 21:06:46 +0700

    memasukkan variabel PBO

--- project.java
+++ project.java
@@ -4,7 +4,16 @@ public class project {
     public static void main(String[] args) {
         Scanner newScanner = new Scanner(System.in);
 
-    System.out.println("masukkan nilai ");
+    System.out.println("masukkan nilai StrukturD= ");
+    double StrukturD = newScanner.nextDouble();
+
+    System.out.println("masukkan nilai PBO ");
+    double PBO = newScanner.nextDouble();
+
+    
+
+
+
     }
 
 }
