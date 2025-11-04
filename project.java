d49bded9284a442f83cbcee9bcd9fc50a99e12e2
rendyalfiansyah <rendyalfiasyah356@gmail.com>
2025-11-04 21:01:14 +0700

    masukkan nilai StrukturD

--- project.java
+++ project.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class project {
     public static void main(String[] args) {
-        
+        Scanner newScanner = new Scanner(System.in);
+
+    System.out.println("masukkan nilai ");
     }
 
 }
