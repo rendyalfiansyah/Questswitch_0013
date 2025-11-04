715c4f8c5c4f9692623c3f7d50d1bb28a3009aa0
rendyalfiansyah <rendyalfiasyah356@gmail.com>
2025-11-04 21:49:20 +0700

    masukkan nilai dan predikat rata - rata

--- project.java
+++ project.java
@@ -18,6 +18,18 @@ public class project {
 
     double ratarata = (StrukturD + PBO + algoritm + RPL)/5;String predikat;
 
+     if (ratarata >= 3.75) {
+                  System.out.println("predikat = A (sangat baik)");
+               } else if (ratarata >= 3.50) {
+                  System.out.println("predikat = AB (baik sekali)");
+               } else if (ratarata >=3.00)  {
+                  System.out.println("predikat = B (baik)");
+               } else if (ratarata >= 2.50) {
+                  System.out.println("predikat = BC (cukup)");
+               } else if (ratarata <=2.50) {
+                  System.out.println("predikat = C (kurang)");
+               } 
+
 
 
     
